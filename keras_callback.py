44ccf3bcd5765557a4ff831137000acd8fda8d13
Jakob Aungiers <jakob@jakob-aungiers.com>
2016-10-18 10:19:30 +0100

    added autosave model function on lower loss

--- keras_callback.py
+++ keras_callback.py
@@ -1,13 +1,15 @@
 import sys
 import time
 import keras
+import keras_io
  
 class CustomCallback(keras.callbacks.Callback):
 
-    def __init__(self, total_epochs, epoch_multiple = 1):
+    def __init__(self, total_epochs, epoch_multiple = 1, autosave = True):
         self.losses = []
         self.epoch_multiple = epoch_multiple
         self.total_epochs = total_epochs
+        self.autosave = autosave
 
     def on_train_begin(self, logs={}):
         print '\n### Training Model ###'
@@ -23,6 +25,7 @@ class CustomCallback(keras.callbacks.Callback):
  
     def on_epoch_end(self, epoch, logs={}):
         if (epoch == 0):
+            self.losses.append(logs.get('loss'))
             return #Don't care about 0 progress and don't want to divide by 0 in ETA calcs
 
         if (epoch % self.epoch_multiple == 0):
@@ -35,17 +38,23 @@ class CustomCallback(keras.callbacks.Callback):
             eta_mins = int(eta)/60
             eta_hrs  = eta_mins/60
             eat_sec  = int(eta) % 60
+            eta_mins = eta_mins-(eta_hrs*60)
             sys.stdout.write('Epoch ' +
                 str(epoch) + '/' +
                 str(self.total_epochs) + ' (' + format(pctDone, '0.1f') + '%) | ETA: ' + str(eta_hrs) + 'h:' + str(eta_mins) + 'm:' + str(eat_sec) + 's | Loss:' +
                 str(logs.get('loss')) + '\r')
             sys.stdout.flush()
 
+        #Save model if log loss is less than prior
+        if (self.autosave) and (logs.get('loss') < self.losses[-1]):
+            print ''
+            keras_io.save_model(self.model, 'model_autosave')
+
+        self.losses.append(logs.get('loss'))
         return
  
     def on_batch_begin(self, batch, logs={}):
         return
  
     def on_batch_end(self, batch, logs={}):
-        self.losses.append(logs.get('loss'))
         return
